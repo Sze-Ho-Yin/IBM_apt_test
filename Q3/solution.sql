f4bb8f204288f871d2a4c22883a383af0fe704c0
Derek <derek.fyp@gmail.com>
2022-11-11 20:11:21 +0800

    update

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -3,4 +3,4 @@ FROM owner, article, category_aricle_mapping, category
 WHERE owner.owner_id==article.owner_id AND category_aricle_mapping.article_id==article.article_id 
 AND category.category_id == category_aricle_mapping.category_id
 GROUP_BY owner_id, category_id
-ORDER_BY COUNT(cateog0ry_id)
+ORDER_BY COUNT(cateogory_id)

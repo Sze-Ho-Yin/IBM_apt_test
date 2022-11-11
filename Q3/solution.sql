018a85ac65e15da8fc674ef34f930d5ff3efb650
Derek <derek.fyp@gmail.com>
2022-11-11 20:09:13 +0800

    update on 13

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -1,6 +1,6 @@
-SELECT owner_id, owner_name, COUNT()
+SELECT owner_id, owner_name, COUNT(category_id)
 FROM owner, article, category_aricle_mapping, category
 WHERE owner.owner_id==article.owner_id AND category_aricle_mapping.article_id==article.article_id 
 AND category.category_id == category_aricle_mapping.category_id
-GROUP_BY owner_id
-ORDER_BY
+GROUP_BY owner_id, category_id
+ORDER_BY COUNT(cateog0ry_id)
